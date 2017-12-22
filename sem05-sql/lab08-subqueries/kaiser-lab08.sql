f97346e26a0e88209b7af6a962653a32ff5b644e
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-12-22 10:02:04 +0100

    Add lab08-subqueries

---
+++ sem05-sql/lab08-subqueries/kaiser-lab08.sql
@@ -0,0 +1,58 @@
+USE companydb;
+
+-- Task 1
+
+SELECT e_empno, e_ename, e_job, e_sal FROM e_emp
+WHERE e_job = (SELECT e_job FROM e_emp WHERE e_ename = "allen");
+
+-- Task 2
+
+SELECT e_empno, e_ename, e_job, e_sal FROM e_emp
+WHERE e_job IN (
+  SELECT e_job FROM e_emp
+  WHERE e_ename IN ("scott", "jones", "king")
+);
+
+-- Task 3
+
+SELECT e_empno, e_ename, e_job, e_sal, e_hiredate FROM e_emp
+WHERE e_hiredate < (
+  SELECT min(e_hiredate) FROM e_emp
+  INNER JOIN d_dept ON e_d_deptno = d_deptno
+  WHERE d_dname = "sales" 
+);
+
+-- another solution
+
+-- SELECT e_empno, e_ename, e_job, e_sal, e_hiredate FROM e_emp
+-- WHERE e_hiredate < ALL (
+--   SELECT e_hiredate FROM e_emp
+--   INNER JOIN d_dept ON e_d_deptno = d_deptno
+--   WHERE d_dname = "sales"
+-- );
+
+-- Task 4
+
+SELECT e_empno, e_ename, e_job, e_sal FROM e_emp
+WHERE e_job = (SELECT e_job FROM e_emp WHERE e_ename = "allen");
+
+-- Task 5
+
+SELECT e_empno, e_ename, e_job, e_sal, e_e_mgr FROM e_emp
+WHERE e_e_mgr = (SELECT e_e_mgr FROM e_emp WHERE e_ename = "allen")
+  AND e_sal <= (SELECT e_sal FROM e_emp WHERE e_ename = "turner");
+
+-- Task 6
+
+UPDATE e_emp SET e_sal = 2850
+WHERE e_empno = 7782;
+
+
+SELECT e_empno, e_ename, e_job, e_sal FROM e_emp
+INNER JOIN d_dept ON e_d_deptno = d_deptno
+WHERE d_dname = "accounting"
+  AND e_sal = ANY (
+    SELECT e_sal FROM e_emp
+    INNER JOIN d_dept ON e_d_deptno = d_deptno
+    WHERE d_dname = "sales"
+  );
