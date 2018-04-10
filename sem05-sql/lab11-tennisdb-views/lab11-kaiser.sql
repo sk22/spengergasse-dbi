8c044294ae4d471e4df409380f47770d3807bab9
Samuel Kaiser <samuel.kaiser01@gmail.com>
2018-04-10 16:47:52 +0200

    Add task 3 and 4

--- sem05-sql/lab11-tennisdb-views/lab11-kaiser.sql
+++ sem05-sql/lab11-tennisdb-views/lab11-kaiser.sql
@@ -3,12 +3,30 @@ USE tennisdb;
 -- Task 1
 
 DROP VIEW IF EXISTS towns_view;
+
 CREATE VIEW towns_view AS
-  SELECT DISTINCT p_town FROM p_players;
+SELECT DISTINCT p_town FROM p_players;
 
 -- Task 2
 
 DROP VIEW IF EXISTS players_league_view;
+
 CREATE VIEW players_league_view AS
-  SELECT p_playerno, p_leagueno FROM p_players
-  WHERE p_leagueno IS NOT NULL;
+SELECT p_playerno, p_leagueno FROM p_players
+WHERE p_leagueno IS NOT NULL;
+
+-- Task 3
+
+DELETE FROM players_league_view
+WHERE p_leagueno = 7060;
+
+-- Task 4
+
+DROP VIEW IF EXISTS league_numbers_view;
+
+CREATE VIEW league_numbers_view AS
+SELECT DISTINCT p_leagueno FROM players_league_view;
+
+-- Task 5
+
+
