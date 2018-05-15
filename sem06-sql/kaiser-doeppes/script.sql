026d78ff12bb1cb3ce4ff836a45cdbaef113140e
Samuel Kaiser <samuel.kaiser01@gmail.com>
2018-05-15 17:03:26 +0200

    Add task 3

--- sem06-sql/kaiser-doeppes/script.sql
+++ sem06-sql/kaiser-doeppes/script.sql
@@ -15,6 +15,7 @@ WHERE a_avail_balance < (
   )
 );
 
+
 # Task 2
 
 SELECT * FROM c_customers
@@ -22,3 +23,14 @@ WHERE (
   SELECT count(a_account_nr) FROM a_accounts
   WHERE a_c_cust_nr = c_cust_nr
 ) = 2;
+
+
+# Task 3
+
+DROP VIEW IF EXISTS view_abteilungen_mitarbeiter;
+CREATE VIEW view_abteilungen_mitarbeiter AS
+SELECT d_dept_nr, d_name, count(e_emp_nr) AS mitarbeiter_anzahl FROM e_employees
+INNER JOIN d_departments ON e_d_dept_nr = d_dept_nr
+GROUP BY e_d_dept_nr;
+
+SELECT * FROM view_abteilungen_mitarbeiter;
